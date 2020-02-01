bf4d4119233b627b4f7849bc898ce2221d20a3bd
Vasco Ramos <vascoalramos@outlook.pt>
2020-02-01 18:44:06 +0000

    :construction: Started working on the featured projects section

---
+++ src/components/featured/feature.jsx
@@ -0,0 +1,197 @@
+import React, { useEffect, useRef } from 'react';
+import PropTypes from 'prop-types';
+import Img from 'gatsby-image';
+import styled from 'styled-components';
+import Title from '../Title/Title';
+import ProjectImg from '../Image/ProjectImg'
+
+const StyledContainer = styled.div`
+  flex-direction: column;
+  align-items: flex-start;
+`;
+const StyledContent = styled.div`
+  position: relative;
+  grid-column: 1 / 7;
+  grid-row: 1 / -1;
+`;
+const StyledLabel = styled.h4`
+  font-weight: normal;
+  color: #4ddbff;
+  font-family: Space Monde;
+  margin-top: 10px;
+  padding-top: 0;
+`;
+const StyledProjectName = styled.h5`
+  font-size: 28px;
+  margin: 0 0 20px;
+`;
+const StyledDescription = styled.div`
+  position: relative;
+  z-index: 2;
+  padding: 25px;
+  p {
+    margin: 0;
+  }
+`;
+const StyledTechList = styled.ul`
+  position: relative;
+  z-index: 2;
+  display: flex;
+  flex-wrap: wrap;
+  padding: 0;
+  margin: 25px 0 10px;
+  list-style: none;
+  li {
+    font-family: Space Mono;
+    margin-bottom: 7px;
+    white-space: nowrap;
+    &:last-of-type {
+      margin-right: 0;
+    }
+  }
+`;
+const StyledLinkWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  position: relative;
+  margin-top: 10px;
+  margin-left: -10px;
+  a {
+    padding: 10px;
+    svg {
+      width: 22px;
+      height: 22px;
+    }
+  }
+`;
+const StyledFeaturedImg = styled(Img)`
+  width: 100%;
+  max-width: 100%;
+  position: relative;
+  mix-blend-mode: multiply;
+  filter: grayscale(100%) contrast(1) brightness(90%);
+`;
+const StyledImgContainer = styled.a`
+  grid-column: 6 / -1;
+  grid-row: 1 / -1;
+  position: relative;
+  z-index: 1;
+  &:hover,
+  &:focus {
+    background: transparent;
+    &: before;
+  }
+  &:before {
+    content: '';
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    z-index: 3;
+    mix-blend-mode: screen;
+  }
+`;
+const StyledProject = styled.div`
+  display: grid;
+  grid-gap: 10px;
+  grid-template-columns: repeat(12, 1fr);
+  align-items: center;
+  margin-bottom: 100px;
+  &:last-of-type {
+    margin-bottom: 0;
+  }
+  &:nth-of-type(odd) {
+  }
+`;
+
+const Featured = ({ data }) => {
+  const featuredProjects = data.filter(({ node }) => node);
+
+  const revealTitle = useRef(null);
+  const revealProjects = useRef([]);
+
+  console.log(data);    
+
+  return (
+    <StyledContainer id="projects">
+      <Title title="Featured Projects" />
+
+      <div>
+        {featuredProjects &&
+          featuredProjects.map(({ node }, i) => {
+            const { frontmatter, html } = node;
+            const { external, title, tech, github, image } = frontmatter;
+
+            return (
+              <StyledProject key={i} ref={el => (revealProjects.current[i] = el)}>
+                <StyledContent>
+                  <StyledLabel>Featured Project</StyledLabel>
+                  <StyledProjectName>
+                    {external ? (
+                      <a
+                        href={external}
+                        target="_blank"
+                        rel="nofollow noopener noreferrer"
+                        aria-label="External Link"
+                      >
+                        {title}
+                      </a>
+                    ) : (
+                      title
+                    )}
+                  </StyledProjectName>
+                  <StyledDescription dangerouslySetInnerHTML={{ __html: html }} />
+                  {tech && (
+                    <StyledTechList>
+                      {tech.map((tech, i) => (
+                        <li key={i}>{tech}</li>
+                      ))}
+                    </StyledTechList>
+                  )}
+                  <StyledLinkWrapper>
+                    {github && (
+                      <a
+                        href={github}
+                        target="_blank"
+                        rel="nofollow noopener noreferrer"
+                        aria-label="GitHub Link"
+                      >
+                        <i className={`fa fa-github fa-inverse`} />
+                      </a>
+                    )}
+                    {external && (
+                      <a
+                        href={external}
+                        target="_blank"
+                        rel="nofollow noopener noreferrer"
+                        aria-label="External Link"
+                      >
+                        <i className={`fa fa-globe fa-inverse`} />
+                      </a>
+                    )}
+                  </StyledLinkWrapper>
+                </StyledContent>
+
+                <StyledImgContainer
+                  href={external ? external : github ? github : '#'}
+                  target="_blank"
+                  rel="nofollow noopener noreferrer"
+                >
+                  <ProjectImg alt={`${title} picture`} filename={image} />
+                </StyledImgContainer>
+              </StyledProject>
+            );
+          })}
+      </div>
+    </StyledContainer>
+  );
+};
+
+Featured.propTypes = {
+  data: PropTypes.array.isRequired,
+};
+
+export default Featured;
---
+++ src/data/featured/intrusion-tracker/index.md
@@ -0,0 +1,21 @@
+---
+date: '1'
+title: 'Intrusion Tracker'
+image: './it.png'
+github: 'https://github.com/vascoalramos/intrusion-tracker'
+external: 'http://192.168.160.220:3000'
+tech:
+  - ReactJs
+  - React Native
+  - Java
+  - Spring Boot Framework
+  - Python
+  - RabbitMQ
+  - MySQL
+  - Twilio SendGrid
+  - GitLab CI/CD
+  - Jira
+showInProjects: true
+---
+
+A management system capable of logging all accesses to a company building and capable of performing the right action when confronted with a strange/suspicious access. It also includes activity reports, statistics and alerts on those actions.
