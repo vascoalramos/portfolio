28c02034c2facb2a78589b334dbacc08c3ffc7d1
Vasco Ramos <vascoalramos@outlook.pt>
2020-02-02 21:17:30 +0000

    :mag: Updating website siteMetadata's description

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,8 @@ module.exports = {
   siteMetadata: {
     lang: 'en',
     title: 'Vasco Ramos | Computer Science Student',
-    description: "I'm Vasco, a 20 years old Computer Science Student at UA, from Portugal.",
+    description:
+      "I'm Vasco Ramos, a 20-year-old Computer Science and Software Engineering student at the University of Aveiro, from Portugal.",
     author: 'Vasco Ramos',
     siteUrl: 'https://vascoalramos.me',
     imageShare: 'share.png',
@@ -10,7 +11,7 @@ module.exports = {
     social: [
       { name: 'linkedin', url: 'https://linkedin.com/in/vascoalramos' },
       { name: 'github', url: 'https://github.com/vascoalramos' },
-    ]
+    ],
   },
   plugins: [
     'gatsby-plugin-sass',
@@ -24,28 +25,28 @@ module.exports = {
       options: {
         name: 'content',
         path: `${__dirname}/content/`,
-      }
+      },
     },
     {
       resolve: 'gatsby-source-filesystem',
       options: {
         name: 'images',
         path: `${__dirname}/src/images/`,
-      }
+      },
     },
     {
       resolve: 'gatsby-source-filesystem',
       options: {
         name: 'pages',
         path: `${__dirname}/src/pages/`,
-      }
+      },
     },
     {
       resolve: 'gatsby-source-filesystem',
       options: {
         name: 'src',
         path: `${__dirname}/src/`,
-      }
+      },
     },
     {
       resolve: `gatsby-plugin-gtag`,
@@ -53,7 +54,7 @@ module.exports = {
         trackingId: `UA-156556339-1`,
         head: true,
         anonymize: true,
-      }
+      },
     },
     {
       resolve: 'gatsby-plugin-manifest',
@@ -65,7 +66,7 @@ module.exports = {
         theme_color: '#02aab0',
         display: 'standalone',
         icon: 'static/favicon.png',
-      }
+      },
     },
     'gatsby-plugin-sitemap',
     'gatsby-plugin-robots-txt',
