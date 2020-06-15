0d15ea51c7620ed7ff7209a2a0e73a3044ce5b60
Vasco Ramos <vascoalramos@outlook.pt>
2020-06-15 14:44:25 +0100

    Add basic blog structure

---
+++ src/pages/blog/index.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import { graphql, useStaticQuery } from 'gatsby';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../../style/main.scss';
+import { Layout } from '../../templates';
+import { Hero, About, Feature } from '../../components';
+
+export default () => {
+  const data = useStaticQuery(graphql`
+    {
+      featured: allMarkdownRemark(
+        filter: { fileAbsolutePath: { regex: "/featured/" } }
+        sort: { fields: [frontmatter___date], order: DESC }
+      ) {
+        edges {
+          node {
+            frontmatter {
+              title
+              image {
+                childImageSharp {
+                  fluid(maxWidth: 700, quality: 90, traceSVG: { color: "#64ffda" }) {
+                    ...GatsbyImageSharpFluid_withWebp_tracedSVG
+                  }
+                }
+              }
+              tech
+              github
+              gitlab
+              external
+              showInProjects
+            }
+            html
+          }
+        }
+      }
+      hero: markdownRemark(fileAbsolutePath: { regex: "/hero.md/" }) {
+        frontmatter {
+          salute
+          name
+          micro_bio
+          cta
+        }
+      }
+      about: markdownRemark(fileAbsolutePath: { regex: "/about.md/" }) {
+        html
+        frontmatter {
+          image {
+            childImageSharp {
+              fixed(width: 300) {
+                ...GatsbyImageSharpFixed
+              }
+            }
+          }
+          resume
+        }
+      }
+    }
+  `);
+
+  return (
+    <>
+      <Layout>
+        <Hero data={data.hero.frontmatter} />
+        <About data={data.about} />
+        <Feature data={data.featured.edges} />
+        {/* <Projects /> */}
+      </Layout>
+    </>
+  );
+};
